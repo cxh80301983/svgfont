b7b0eeb3cee02003207eccaaf2497844b718c845
cuixiaohan <80301983@qq.com>
2015-12-02 18:30:19 +0800

    updata

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
 /* 
-* @Author: willclass
-* @Date:   2015-12-02 15:17:13
-* @Last Modified by:   willclass
-* @Last Modified time: 2015-12-02 15:26:37
-*/
+ * @Author: willclass
+ * @Date:   2015-12-02 15:17:13
+ * @Last Modified by:   willclass
+ * @Last Modified time: 2015-12-02 18:08:45
+ */
 
 'use strict';
 
 var gulp = require("gulp");
 var iconfont = require('gulp-iconfont');
-var runTimestamp = Math.round(Date.now()/1000);
- 
-gulp.task('font', function(){
-  return gulp.src(['test/SVG/*.svg'])
-    .pipe(iconfont({
-      fontName: 'myfont', // required 
-      appendUnicode: true, // recommended option 
-      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available 
-      timestamp: runTimestamp, // recommended to get consistent builds when watching files 
-    }))
-      .on('glyphs', function(glyphs, options) {
-        // CSS templating, e.g. 
-        console.log(glyphs, options);
-      })
-    .pipe(gulp.dest('www/fonts/'));
+var consolidate = require('gulp-consolidate');
+var runTimestamp = Math.round(Date.now() / 1000);
+
+gulp.task('font', function() {
+	return gulp.src(['test/*.svg'])
+		.pipe(iconfont({
+			fontName: 'myfont', // required 
+			appendUnicode: false, // recommended option 
+			formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available 
+			timestamp: runTimestamp // recommended to get consistent builds when watching files 
+		}))
+		.on('glyphs', function(glyphs, options) {
+			gulp.src('templates/font2.css')
+				.pipe(consolidate('lodash', {
+					glyphs: glyphs,
+					fontName: 'myfont',
+					fontPath: '../fonts/',
+					className: 's'
+				}))
+				.pipe(gulp.dest('www/css/'));
+		})
+		.pipe(gulp.dest('www/fonts/'));
 });
