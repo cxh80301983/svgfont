8184c0d0aad4063941d337d60de3eb9cbe235943
cuixiaohan <80301983@qq.com>
2016-02-26 20:50:13 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: willclass
  * @Date:   2015-12-02 15:17:13
  * @Last Modified by:   willclass
- * @Last Modified time: 2015-12-03 09:54:30
+ * @Last Modified time: 2015-12-07 16:39:48
  */
 
 'use strict';
@@ -16,22 +16,23 @@ var runTimestamp = Math.round(Date.now() / 1000);
 gulp.task('font', function() {
 	return gulp.src(['test/SVG/*.svg'])
 		.pipe(iconfont({
-			fontName: 'myfont', // required 
-			appendUnicode: false, // recommended option 
-			formats: ['ttf', 'eot', 'woff', 'svg'], // default, 'woff2' and 'svg' are available 
+			fontName: 'huitong', // required 
+			// startUnicode:"E9",
+			appendUnicode: true, // recommended option 
+			formats: ['ttf', 'eot', 'woff', 'svg','woff2'], // default, 'woff2' and 'svg' are available 
 			timestamp: runTimestamp // recommended to get consistent builds when watching files 
 		}))
 		.on('glyphs', function(glyphs, options) {
-			gulp.src('templates/font2.css')
+			gulp.src('templates/font.css')
 				.pipe(consolidate('lodash', {
 					glyphs: glyphs,
-					fontName: 'myfont',
+					fontName: 'huitong',
 					fontPath: '../fonts/',
-					className: 's'
+					className: 'ht'
 				}))
-				.pipe(gulp.dest('www/css/'));
+				.pipe(gulp.dest('dist/css/'));
 		})
-		.pipe(gulp.dest('www/fonts/'));
+		.pipe(gulp.dest('dist/fonts/'));
 });
 
 
